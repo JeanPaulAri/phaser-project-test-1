8f6b34c6eb12b982e3ea3eef93b39de34e7892d4
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-10-14 18:57:00 -0500

    dev/ implementing ground gameobject

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -21,8 +21,14 @@ export default class HelloWorldScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
     this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(400, 568, 'ground');
+    const ground = this.platforms.create(400, 568, 'ground') as
+      Phaser.Physics.Arcade.Sprite;
+    ground
+      .setScale(2)
+      .refreshBody();
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
   }
 }
